1834b7c75a5f27a05392da8a18b6e8187dc05b72
Saraesp <saraesposito145@gmail.com>
2023-01-21 19:47:06 +0100

    Correzione js console.log

--- js/script.js
+++ js/script.js
@@ -1,26 +1,30 @@
-
 // 1 - Chiedere all'utente quanti km deve percorrere
-let km = prompt("Quanti chilometri uoi percorrere?");
+let km = prompt("Quanti chilometri vuoi percorrere?");
 
 // 2 - Chiedere l'età del passeggero
 let eta = prompt("Inserisci la tua età");
 
 // 3.1 - Calcolore il prezzo del biglietto 
 let prezzo = 0.21 * km;
+console.log(prezzo);
 
 // 3.2 - Applicazione sconto al biglietto in base all'età
-if(età < 18){
-    let sconto = prezzzo * 20 / 100;
+if(eta < 18){
+    let sconto = prezzo * 20 / 100;
+    console.log(sconto);
     prezzo = prezzo - sconto;
-    console.log("Il prezzo del biglietto è scontato per minorenni ed è pari a: " +prezzo)
+    prezzo = prezzo.toFixed(2);
+    console.log(`Il prezzo del biglietto è scontato per minorenni è pari a:  ${prezzo}`)
 }
 
-else if(età > 65){
-    let sconto = prezzzo * 40 / 100;
+else if(eta > 65){
+    let sconto = prezzo * 40 / 100;
     prezzo = prezzo - sconto;
-    console.log("Il prezzo del biglietto è scontato per over 65 ed è pari a: " +prezzo)
+    prezzo = prezzo.toFixed(2);
+    console.log(`Il prezzo del biglietto è scontato per over 65 è pari a:  ${prezzo}`)
 }
 
 else{
-    console.log("Il prezzo del biglietto è pari a : " +prezzo)
+    prezzo = prezzo.toFixed(2);
+    console.log(`Il prezzo del biglietto è pari a :  ${prezzo}`);
 }
